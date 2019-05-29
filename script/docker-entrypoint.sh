b60dda0324f9fec6b5cf7399f54d76154ea757af
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-05-29 18:48:29 +0200

    fix it

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -53,13 +53,16 @@ if [ "$(stat -c "%Y" "${JIRA_INSTALL}/conf/server.xml")" -eq "0" ]; then
         -i //ConnectorTMP -t attr -n "sslEnabledProtocols" -v "$JIRA_SSL_ENABLE_PROTOCOLS" \
         -i //ConnectorTMP -t attr -n "useBodyEncodingForURI" -v "true" \
         -i //ConnectorTMP -t attr -n "keystoreType" -v "JKS" \
+        -i //ConnectorTMP -t attr -n "relaxedQueryChars" -v "[]|" \
+        -i //ConnectorTMP -t attr -n "relaxedPathChars" -v "[]|{}^&#x5c;&#x60;&quot;&lt;&gt;" \
         -r //ConnectorTMP -v Connector \
         "${JIRA_INSTALL}/conf/server.xml"
 
      xmlstarlet ed -P -S -L --update '//Connector[@port="8080"]/@redirectPort'  -v "8443" "${JIRA_INSTALL}/conf/server.xml"
-     xmlstarlet ed -P -S -L -d '//Connector[@port="8080"]/@relaxedQueryChars' "${JIRA_INSTALL}/conf/server.xml"
-     xmlstarlet ed -P -S -L -d '//Connector[@port="8080"]/@relaxedPathChars'  "${JIRA_INSTALL}/conf/server.xml"
-     xmlstarlet ed -P -S -L -d '//Connector[@port="8080"]/@bindOnInit'  "${JIRA_INSTALL}/conf/server.xml"
+     xmlstarlet ed -P -S -L -u '//Connector[@port="8080"]/@relaxedQueryChars'  --value "[]|" "${JIRA_INSTALL}/conf/server.xml"
+     xmlstarlet ed -P -S -L -u '//Connector[@port="8080"]/@relaxedPathChars' --value "[]|{}^&#x5c;&#x60;&quot;&lt;&gt;" "${JIRA_INSTALL}/conf/server.xml"
+
+     sed 's/\&amp;/\&/g' > "${JIRA_INSTALL}/conf/server.xml"
 
     xmlstarlet ed -P -S -L -N x="http://java.sun.com/xml/ns/javaee" -s "/x:web-app" -t elem -n "security-constraintTMP" -v "" \
         -s "/x:web-app/security-constraintTMP" -t elem -n "web-resource-collectionTMP" -v "" \
