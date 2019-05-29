62571cf8930262bb94c9533487aaf0d3d08bac03
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-05-29 16:27:46 +0200

    escape &

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -56,9 +56,9 @@ if [ "$(stat -c "%Y" "${JIRA_INSTALL}/conf/server.xml")" -eq "0" ]; then
         -r //ConnectorTMP -v Connector \
         "${JIRA_INSTALL}/conf/server.xml"
 
-     xmlstarlet ed -P -S -L --update '//Connector[@port="8080"]' --type "attr" --name "redirectPort" --value "8443" "${JIRA_INSTALL}/conf/server.xml"
-     xmlstarlet ed -P -S -L --update '//Connector[@port="8080"]' --type "attr" --name "relaxedQueryChars" --value "[]|" "${JIRA_INSTALL}/conf/server.xml"
-     xmlstarlet ed -P -S -L --update '//Connector[@port="8080"]' --type "attr" --name "relaxedPathChars" --value "[]|{}^&#x5c;&#x60;&quot;&lt;&gt;" "${JIRA_INSTALL}/conf/server.xml"
+     xmlstarlet ed -P -S -L --update '//Connector[@port="8080"]/@redirectPort'  -v "8443" "${JIRA_INSTALL}/conf/server.xml"
+     xmlstarlet ed -P -S -L -u '//Connector[@port="8080"]/@relaxedQueryChars'  --value "[]|" "${JIRA_INSTALL}/conf/server.xml"
+     xmlstarlet ed -P -S -u '//Connector[@port="8080"]/@relaxedPathChars' --value "[]|{}^&#x5c;&#x60;&quot;&lt;&gt;" "${JIRA_INSTALL}/conf/server.xml" | sed 's/\&amp;/\&/g' > "${JIRA_INSTALL}/conf/server.xml"
 
     xmlstarlet ed -P -S -L -N x="http://java.sun.com/xml/ns/javaee" -s "/x:web-app" -t elem -n "security-constraintTMP" -v "" \
         -s "/x:web-app/security-constraintTMP" -t elem -n "web-resource-collectionTMP" -v "" \
