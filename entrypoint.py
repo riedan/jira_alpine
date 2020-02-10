9d33d4664a6fb44a34fb96e91f729a131f0b2587
riedan <50929933+riedan@users.noreply.github.com>
2020-02-10 14:59:10 +0100

    add chown for data

--- entrypoint.py
+++ entrypoint.py
@@ -80,9 +80,13 @@ except:
     print("Unexpected error:")    
 
 try:
+    shutil.chown(f'{JIRA_HOME}/data', user=RUN_USER, group=RUN_GROUP)
+except:
+    print("Unexpected error:")        
+    
+try:
     shutil.chown(JIRA_HOME, user=RUN_USER, group=RUN_GROUP)
     shutil.chown(f'{JIRA_HOME}/data/attachments', user=RUN_USER, group=RUN_GROUP)
-    shutil.chown(f'{JIRA_HOME}/data', user=RUN_USER, group=RUN_GROUP)
 except:
     print("Unexpected error:")
 
@@ -90,6 +94,7 @@ gen_cfg('server.xml.j2', f'{JIRA_INSTALL_DIR}/conf/server.xml')
 
 gen_cfg('dbconfig.xml.j2', f'{JIRA_HOME}/dbconfig.xml',
         user=RUN_USER, group=RUN_GROUP, overwrite=False)
+
 if str2bool(env.get('clustered')):
     gen_cfg('cluster.properties.j2', f'{JIRA_HOME}/cluster.properties',
             user=RUN_USER, group=RUN_GROUP, overwrite=False)
